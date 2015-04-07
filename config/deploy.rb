6c45f8b5af5f5de44e1897e1ed76e3c1c8cdc6d9
thien <thien.hoviet@gmail.com>
2015-04-07 11:51:26 +0700

    Update github url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,14 @@
 lock '3.3.5'
 
 set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:thienhv/blog-deploy.git'
+
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/dev/my_app_name'
 
 # Default value for :scm is :git
 # set :scm, :git
